399ef3c2a075acdacd230414c8fb503cc6396718
z0z0r4 <z0z0r4@outlook.com>
2025-01-26 09:20:25 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,4 +6,8 @@ from app import APP
 
 @pytest.fixture(scope="session", autouse=True)
 def client():
-    return TestClient(APP)
+    # return TestClient(APP)
+    # You should use TestClient as a context manager, to ensure that the lifespan is called.
+    # https://www.starlette.io/lifespan/#running-lifespan-in-tests
+    with TestClient(app=APP) as client:
+        yield client
