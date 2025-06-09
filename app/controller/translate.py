2f774942eb6c8f90a1b3ffc880faec4627e5d185
z0z0r4 <z0z0r4@outlook.com>
2025-06-10 06:52:44 +0800

    fix: 调换 translate route 注册顺序以把已弃用接口置底

--- app/controller/translate.py
+++ app/controller/translate.py
@@ -13,33 +13,14 @@ from app.utils.response import (
 
 translate_router = APIRouter(prefix="/translate", tags=["translate"])
 
+
 class CurseForgeBatchRequest(BaseModel):
     modIds: Annotated[List[int], Field(min_length=1, max_length=1000)]
+
 
 class ModrinthBatchRequest(BaseModel):
     project_ids: Annotated[List[str], Field(min_length=1, max_length=1000)]
 
-@translate_router.get(
-    "/modrinth",
-    description="Modrinth 翻译",
-    response_model=ModrinthTranslation,
-    deprecated=True
-)
-@cache(expire=3600 * 24)
-async def modrinth_translate(
-    request: Request,
-    project_id: str = Query(..., description="Modrinth Project id"),
-):
-    result: Optional[
-        ModrinthTranslation
-    ] = await request.app.state.aio_mongo_engine.find_one(
-        ModrinthTranslation, ModrinthTranslation.project_id == project_id
-    )
-
-    if result:
-        return TrustableResponse(content=result)
-    else:
-        return UncachedResponse()
 
 @translate_router.get(
     "/modrinth/{project_id}",
@@ -62,6 +43,7 @@ async def modrinth_translate_path(
     else:
         return UncachedResponse()
 
+
 @translate_router.post(
     "/modrinth",
     description="批量 Modrinth 翻译",
@@ -69,10 +51,11 @@ async def modrinth_translate_path(
 )
 async def modrinth_translate_batch(
     request: Request,
-    project_ids: ModrinthBatchRequest = Body(..., description="Modrinth Project ids")
+    project_ids: ModrinthBatchRequest = Body(..., description="Modrinth Project ids"),
 ):
     results: List[ModrinthTranslation] = await request.app.state.aio_mongo_engine.find(
-        ModrinthTranslation, query.in_(ModrinthTranslation.project_id, project_ids.project_ids)
+        ModrinthTranslation,
+        query.in_(ModrinthTranslation.project_id, project_ids.project_ids),
     )
 
     if results:
@@ -80,28 +63,6 @@ async def modrinth_translate_batch(
     else:
         return UncachedResponse()
 
-
-@translate_router.get(
-    "/curseforge",
-    description="CurseForge 翻译",
-    response_model=CurseForgeTranslation,
-    deprecated=True
-)
-@cache(expire=3600 * 24)
-async def curseforge_translate(
-    request: Request,
-    modId: int = Query(..., description="CurseForge Mod id"),
-):
-    result: Optional[
-        CurseForgeTranslation
-    ] = await request.app.state.aio_mongo_engine.find_one(
-        CurseForgeTranslation, CurseForgeTranslation.modId == modId
-    )
-
-    if result:
-        return TrustableResponse(content=result)
-    else:
-        return UncachedResponse()
 
 @translate_router.get(
     "/curseforge/{modId}",
@@ -125,7 +86,6 @@ async def curseforge_translate_path(
         return UncachedResponse()
 
 
-
 @translate_router.post(
     "/curseforge",
     description="批量 CurseForge 翻译",
@@ -133,13 +93,61 @@ async def curseforge_translate_path(
 )
 async def curseforge_translate_batch(
     request: Request,
-    modIds: CurseForgeBatchRequest = Body(..., description="CurseForge Mod ids")
+    modIds: CurseForgeBatchRequest = Body(..., description="CurseForge Mod ids"),
 ):
-    results: List[CurseForgeTranslation] = await request.app.state.aio_mongo_engine.find(
+    results: List[
+        CurseForgeTranslation
+    ] = await request.app.state.aio_mongo_engine.find(
         CurseForgeTranslation, query.in_(CurseForgeTranslation.modId, modIds.modIds)
     )
 
     if results:
         return TrustableResponse(content=results)
+    else:
+        return UncachedResponse()
+
+
+@translate_router.get(
+    "/modrinth",
+    description="Modrinth 翻译",
+    response_model=ModrinthTranslation,
+    deprecated=True,
+)
+@cache(expire=3600 * 24)
+async def modrinth_translate(
+    request: Request,
+    project_id: str = Query(..., description="Modrinth Project id"),
+):
+    result: Optional[
+        ModrinthTranslation
+    ] = await request.app.state.aio_mongo_engine.find_one(
+        ModrinthTranslation, ModrinthTranslation.project_id == project_id
+    )
+
+    if result:
+        return TrustableResponse(content=result)
+    else:
+        return UncachedResponse()
+
+
+@translate_router.get(
+    "/curseforge",
+    description="CurseForge 翻译",
+    response_model=CurseForgeTranslation,
+    deprecated=True,
+)
+@cache(expire=3600 * 24)
+async def curseforge_translate(
+    request: Request,
+    modId: int = Query(..., description="CurseForge Mod id"),
+):
+    result: Optional[
+        CurseForgeTranslation
+    ] = await request.app.state.aio_mongo_engine.find_one(
+        CurseForgeTranslation, CurseForgeTranslation.modId == modId
+    )
+
+    if result:
+        return TrustableResponse(content=result)
     else:
         return UncachedResponse()
