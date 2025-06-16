c625e96139f772b77814cae5ecaf7f3011948f0b
z0z0r4 <z0z0r4@outlook.com>
2025-06-16 12:00:29 +0800

    fix: 修复 modrinth_version_ids 和 modrinth_hashes 的过滤逻辑，以免 sadd 无可添加的值

--- app/sync_queue/modrinth.py
+++ app/sync_queue/modrinth.py
@@ -13,30 +13,31 @@ async def add_modrinth_project_ids_to_queue(project_ids: List[str]):
 
 
 async def add_modrinth_version_ids_to_queue(version_ids: List[str]):
-    if len(version_ids) != 0:
+    version_ids = [
+        version_id
+        for version_id in version_ids
+        if re.match(r"[a-zA-Z0-9]{8}", version_id)
+    ]
+    if len(version_ids) > 0:
         await sync_queuq_redis_engine.sadd(
             "modrinth_version_ids",
-            *[
-                version_id
-                for version_id in version_ids
-                if re.match(r"[a-zA-Z0-9]{8}", version_id)
-            ],
+            *version_ids,
         )
 
 
 async def add_modrinth_hashes_to_queue(hashes: List[str], algorithm: str = "sha1"):
     if algorithm not in ["sha1", "sha512"]:
         raise ValueError("algorithm must be one of sha1, sha512")
-    if len(hashes) != 0:
-
+    hashes = [
+        hash
+        for hash in hashes
+        if re.match(
+            r"[a-zA-Z0-9]{40}" if algorithm == "sha1" else r"[a-zA-Z0-9]{128}",
+            hash,
+        )
+    ]
+    if len(hashes) > 0:
         await sync_queuq_redis_engine.sadd(
             f"modrinth_hashes_{algorithm}",
-            *[
-                hash
-                for hash in hashes
-                if re.match(
-                    r"[a-zA-Z0-9]{40}" if algorithm == "sha1" else r"[a-zA-Z0-9]{128}",
-                    hash,
-                )
-            ],
+            *hashes,
         )
